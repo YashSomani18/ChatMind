e14663cfa6020f84f26dd98cea3f990192539f1e
YashSomani18 <yashsomani18@gmail.com>
2024-01-09 01:38:10 +0530

    "Third commit"

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,9 @@ const app = express();
 //Middlewares
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(cors({origin:"https://localhost:5173" , credentials:true , optionsSuccessStatus:200}));
+app.use(cors({origin:["https://localhost:5173","https://chatmind-b9a0.onrender.com"] 
+
+, credentials:true , optionsSuccessStatus:200}));
 
 
 //remove it in production
