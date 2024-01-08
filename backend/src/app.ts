706b5eb7214938c275b78aea84c753721a75688e
YashSomani18 <yashsomani18@gmail.com>
2024-01-09 03:05:44 +0530

    '11th commit'

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { config } from "dotenv";
-import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -20,8 +19,6 @@ app.use(
   })
 );
 
-//remove it in production
-app.use(morgan("dev"));
 
 app.use("/api/v1", appRouter);
 
